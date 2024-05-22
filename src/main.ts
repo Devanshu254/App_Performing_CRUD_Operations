cc506c24ab9ef64a0e24fcf8b67be1f7b9664430
Devanshu Jha <99939860+Devanshu254@users.noreply.github.com>
2024-05-22 15:27:16 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import * as process from "process";
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(process.env.PORT, '0.0.0.0');
 }
 bootstrap();
